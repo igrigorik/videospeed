33da3f2a5e39814df9217df6e7bc2d1cb38fd9d9
Ilya Grigorik <ilya@igvita.com>
2014-08-24 23:01:03 -0700

    add popup page

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#config').addEventListener('click', function() {
+    window.open(chrome.extension.getURL("options.html"));
+  });
+  
+  document.querySelector('#about').addEventListener('click', function() {
+    window.open("https://github.com/igrigorik/videospeed");
+  });
+
+  document.querySelector('#feedback').addEventListener('click', function() {
+    window.open("https://github.com/igrigorik/videospeed/issues");
+  });
+});
