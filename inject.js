e55c8884e5a97959a43c3c60ac1b3ec66100c39b
Ilya Grigorik <ilya@igvita.com>
2014-11-30 09:33:28 -0800

    beautify js

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
+    if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval);
 
       var tc = tc || {};
@@ -20,7 +20,9 @@ chrome.extension.sendMessage({}, function(response) {
         this.video.addEventListener('ratechange', function(event) {
           var speed = this.getSpeed();
           this.speedIndicator.textContent = speed;
-          chrome.storage.sync.set({'speed': speed});
+          chrome.storage.sync.set({
+            'speed': speed
+          });
         }.bind(this));
       };
 
@@ -63,11 +65,14 @@ chrome.extension.sendMessage({}, function(response) {
         this.speedIndicator = speedIndicator;
 
         container.addEventListener('click', function(e) {
-          if      (e.target === slowerButton) { runAction('slower') }
-          else if (e.target === fasterButton) { runAction('faster') }
-          else if (e.target === rewindButton) { runAction('rewind') }
-          else {
-            container.nextSibling.classList.add("vc-cancelled")
+          if (e.target === slowerButton) {
+            runAction('slower')
+          } else if (e.target === fasterButton) {
+            runAction('faster')
+          } else if (e.target === rewindButton) {
+            runAction('rewind')
+          } else {
+            container.nextSibling.classList.add('vc-cancelled')
             container.remove();
           }
 
@@ -85,7 +90,7 @@ chrome.extension.sendMessage({}, function(response) {
         container.addEventListener('mousedown', function(e) {
           e.preventDefault();
           e.stopPropagation();
-        }, true);        
+        }, true);
       }
 
       function runAction(action) {
@@ -93,29 +98,35 @@ chrome.extension.sendMessage({}, function(response) {
         videoTags.forEach = Array.prototype.forEach;
 
         videoTags.forEach(function(v) {
-          if (!v.paused && !v.classList.contains("vc-cancelled")) {
+          if (!v.paused && !v.classList.contains('vc-cancelled')) {
             if (action === 'rewind') {
               v.playbackRate = Math.max(v.playbackRate - speedStep, 0.00);
               v.currentTime -= rewindTime;
-            } else if (action === 'faster') { 
-                v.playbackRate += speedStep }
-              else if (action === 'slower') { 
-                  v.playbackRate = Math.max(v.playbackRate - speedStep, 0.00); }
+            } else if (action === 'faster') {
+              v.playbackRate += speedStep
+            } else if (action === 'slower') {
+              v.playbackRate = Math.max(v.playbackRate - speedStep, 0.00);
+            }
           }
         });
       }
 
       document.addEventListener('keypress', function(event) {
-        
         // if lowercase letter pressed, check for uppercase key code
         var keyCode = String.fromCharCode(event.keyCode).toUpperCase().charCodeAt();
 
         // Ignore keypress event if typing in an input box
-        if      (document.activeElement.nodeName === "INPUT" && document.activeElement.getAttribute("type") === "text") { return false; }
+        if (document.activeElement.nodeName === 'INPUT' && document.activeElement.getAttribute('type') === 'text') {
+          return false;
+        }
 
-        if      (keyCode == rewindKeyCode) { runAction('rewind') }
-        else if (keyCode == fasterKeyCode) { runAction('faster') } 
-        else if (keyCode == slowerKeyCode) { runAction('slower') }
+        if (keyCode == rewindKeyCode) {
+          runAction('rewind')
+        } else if (keyCode == fasterKeyCode) {
+          runAction('faster')
+        } else if (keyCode == slowerKeyCode) {
+          runAction('slower')
+        }
 
         return false;
       }, true);
@@ -132,23 +143,23 @@ chrome.extension.sendMessage({}, function(response) {
       videoTags.forEach(function(video) {
         var control = new tc.videoController(video);
       });
-        
+
       var speedStep, rewindTime, rewindKeyCode, slowerKeyCode, fasterKeyCode;
-      
-      chrome.storage.sync.get({ 
-        speedStep:        0.1, // default 0.10x
-        rewindTime:       10,   // default 10s
-          rewindKeyCode:  65,   // default: A
-          slowerKeyCode:  83,   // default: S
-          fasterKeyCode:  68    // default: D
-        },              
-        function(storage) { 
-          speedStep     = Number(storage.speedStep);
-          rewindTime    = Number(storage.rewindTime);
+
+      chrome.storage.sync.get({
+          speedStep: 0.1, // default 0.10x
+          rewindTime: 10, // default 10s
+          rewindKeyCode: 65, // default: A
+          slowerKeyCode: 83, // default: S
+          fasterKeyCode: 68 // default: D
+        },
+        function(storage) {
+          speedStep = Number(storage.speedStep);
+          rewindTime = Number(storage.rewindTime);
           rewindKeyCode = Number(storage.rewindKeyCode);
           slowerKeyCode = Number(storage.slowerKeyCode);
           fasterKeyCode = Number(storage.fasterKeyCode);
-      });                     
+        });
     }
   }, 10);
 });
