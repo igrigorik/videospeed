02b2da0f977898be29d11b16b8e3789075b21e91
The Metaist <metaist@metaist.com>
2017-03-16 10:19:14 -0400

    use mouse wheel scroll to increase/decrease speed

--- inject.js
+++ inject.js
@@ -121,6 +121,13 @@ chrome.extension.sendMessage({}, function(response) {
         </div>
       `;
       shadow.innerHTML = shadowTemplate;
+      shadow.querySelector('#controller').addEventListener('wheel', (e) => {
+        runAction(e.wheelDelta > 0 ? 'faster' : 'slower', document);
+        e.preventDefault();
+        e.stopPropagation();
+      });
+
+
       shadow.querySelector('.draggable').addEventListener('mousedown', (e) => {
         runAction(e.target.dataset['action'], document);
       });
