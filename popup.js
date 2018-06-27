ce7be7943718061f16d4b5e75bd839bd586ed031
Can Arslan <canarslan12@hotmail.com>
2018-06-27 16:26:44 +0200

    Allow user to define custom shortcuts - Options page changed (Jquery Added) - Code CleanUp

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#config').addEventListener('click', function() {
-    window.open(chrome.runtime.getURL("options.html"));
+    window.open(chrome.extension.getURL("options.html"));
   });
   
   document.querySelector('#about').addEventListener('click', function() {
