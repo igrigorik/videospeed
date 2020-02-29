e6774b5cbff7958f51fde05a59351ea1e99dfe6a
Chad Bailey <chadbailey904@gmail.com>
2020-02-28 23:20:59 -0600

    Demonstrating autofixed style check

--- examples/sum.test.js
+++ examples/sum.test.js
@@ -3,5 +3,5 @@
 const sum = require("./sum");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2))                                                                     .toBe(3);
 });
