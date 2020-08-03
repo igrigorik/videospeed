d87c863749615ef57170e3b9ccc1c5fbea11c545
newice <newice@gmx.net>
2020-08-03 10:45:17 +0200

    merge with upstream

---
+++ CONTRIBUTING.md
@@ -0,0 +1,85 @@
+# Contributing
+
+Video Speed Controller is an open source project licensed under the MIT license
+with many contributers. Contributions are welcome, and greatly appreciated.
+
+If you would like to help, getting started is easy.
+
+## Get Started
+
+1. You must have a github account and be logged in
+2. Fork the repo by clicking the "Fork" link on the top-right corner of the page
+3. Once the fork is ready, clone to your local PC
+
+   ```sh
+   $ git clone https://github.com/<USERNAME>/videospeed.git
+   Cloning into 'videospeed'...
+    remote: Enumerating objects: 10, done.
+    remote: Counting objects: 100% (10/10), done.
+    remote: Compressing objects: 100% (9/9), done.
+    remote: Total 877 (delta 3), reused 2 (delta 1), pack-reused 867
+    Receiving objects: 100% (877/877), 317.65 KiB | 2.17 MiB/s, done.
+    Resolving deltas: 100% (543/543), done.
+   ```
+
+4. Create a branch for your changes
+
+   ```sh
+    $ cd videospeed
+    videospeed$ git checkout -b bugfix/1-fix-double-click
+    M   .github/workflows/chrome-store-upload.yaml
+    M   README.md
+    M   options.js
+    Switched to a new branch 'bugfix/1-fix-double-click'
+    videospeed$
+   ```
+
+5. Open the code in your favorite code editor, make your changes
+
+   ```sh
+   echo "Awesome changes" > somefile.js
+   git add .
+   ```
+
+   > Important: Your commit must be formatted using
+   > [prettier](https://prettier.io/). If it is not it may be autoformatted for
+   > you or your pull request may be rejected.
+
+6. Next, open Chrome/Brave/Chromium and enable developer mode via
+   `Settings > Extensions > Manage Extensions` and toggle `Developer mode` in
+   the top-right corner.
+7. Click `Load unpacked` and browse to the folder you cloned videospeed to.
+8. Try out your changes, make sure they work as expected
+9. Commit and push your changes to github
+
+   ```sh
+   git commit -m "Awesome description of some awesome changes."
+   git push
+   ```
+
+10. Open your branch up on the github website then click `New pull request` and
+    write up a description of your changes.
+
+## Optional
+
+### Run Pre-Commit Checks Locally
+
+Installing [pre-commit](https://pre-commit.com/) is easy to do (click the link
+for instructions on your platform). This repo comes with pre-commit already
+configured. Doing this will ensure that your project is properly formatted and
+runs some very basic tests. Once you have pre-commit installed on your system,
+simply enter `pre-commit install` in your terminal in the folder to have these
+checks run automatically each time you commit.
+
+Even better, after issueing the install command you can now manually run
+pre-commit checks before committing via `pre-commit run --all-files`
+
+### Pull Upstream Changes
+
+You should always be working with the latest version of the tool to make pull
+requests easy. If you want to do this easily, just add a second remote to your
+local git repo like this
+`git remote add upstream https://github.com/igrigorik/videospeed.git`
+
+Now any time you like to pull the latest version in to your local branch you can
+simply issue the command `git pull upstream master`
--- options.js
+++ options.js
@@ -1,101 +1,102 @@
-var regStrip=/^[\r\t\f\v ]+|[\r\t\f\v ]+$/gm;
+var regStrip = /^[\r\t\f\v ]+|[\r\t\f\v ]+$/gm;
 
 var tcDefaults = {
-  speed: 1.0,           // default:
-  displayKeyCode: 86,   // default: V
+  speed: 1.0, // default:
+  displayKeyCode: 86, // default: V
   rememberSpeed: false, // default: false
   audioBoolean: false, // default: false
-  startHidden: false,   // default: false
-  enabled: true,        // default enabled
+  startHidden: false, // default: false
+  enabled: true, // default enabled
   controllerOpacity: 0.3, // default: 0.3
   keyBindings: [
-    {action: "display", key: 86, value: 0, force: false, predefined: true }, // V
-    {action: "slower", key: 83, value: 0.1, force: false, predefined: true}, // S
-    {action: "faster", key: 68, value: 0.1, force: false, predefined: true}, // D
-    {action: "rewind", key: 90, value: 10, force: false, predefined: true}, // Z
-    {action: "advance", key: 88, value: 10, force: false, predefined: true}, // X
-    {action: "reset", key: 82, value: 1, force: false, predefined: true}, // R
-    {action: "fast", key: 71, value: 1.8, force: false, predefined: true}, // G
-    {action: "adaptive", key: 65, value: 0, force: false, predefined: true} // A
+    { action: "display", key: 86, value: 0, force: false, predefined: true }, // V
+    { action: "slower", key: 83, value: 0.1, force: false, predefined: true }, // S
+    { action: "faster", key: 68, value: 0.1, force: false, predefined: true }, // D
+    { action: "rewind", key: 90, value: 10, force: false, predefined: true }, // Z
+    { action: "advance", key: 88, value: 10, force: false, predefined: true }, // X
+    { action: "reset", key: 82, value: 1, force: false, predefined: true }, // R
+    { action: "fast", key: 71, value: 1.8, force: false, predefined: true }, // G
+    { action: "adaptive", key: 65, value: 3, force: false, predefined: true } // A
   ],
-  blacklist: `
-    www.instagram.com
+  blacklist: `www.instagram.com
     twitter.com
-    vine.co
     imgur.com
     teams.microsoft.com
-  `.replace(regStrip, '')
+  `.replace(regStrip, "")
 };
 
 var keyBindings = [];
 
 var keyCodeAliases = {
-  0: 'null',
-  null: 'null',
-  undefined: 'null',
-  32: 'Space',
-  37: 'Left',
-  38: 'Up',
-  39: 'Right',
-  40: 'Down',
-  96: 'Num 0',
-  97: 'Num 1',
-  98: 'Num 2',
-  99: 'Num 3',
-  100: 'Num 4',
-  101: 'Num 5',
-  102: 'Num 6',
-  103: 'Num 7',
-  104: 'Num 8',
-  105: 'Num 9',
-  106: 'Num *',
-  107: 'Num +',
-  109: 'Num -',
-  110: 'Num .',
-  111: 'Num /',
-  112: 'F1',
-  113: 'F2',
-  114: 'F3',
-  115: 'F4',
-  116: 'F5',
-  117: 'F6',
-  118: 'F7',
-  119: 'F8',
-  120: 'F9',
-  121: 'F10',
-  122: 'F11',
-  123: 'F12',
-  186: ';',
-  188: '<',
-  189: '-',
-  187: '+',
-  190: '>',
-  191: '/',
-  192: '~',
-  219: '[',
-  220: '\\',
-  221: ']',
-  222: '\'',
-}
+  0: "null",
+  null: "null",
+  undefined: "null",
+  32: "Space",
+  37: "Left",
+  38: "Up",
+  39: "Right",
+  40: "Down",
+  96: "Num 0",
+  97: "Num 1",
+  98: "Num 2",
+  99: "Num 3",
+  100: "Num 4",
+  101: "Num 5",
+  102: "Num 6",
+  103: "Num 7",
+  104: "Num 8",
+  105: "Num 9",
+  106: "Num *",
+  107: "Num +",
+  109: "Num -",
+  110: "Num .",
+  111: "Num /",
+  112: "F1",
+  113: "F2",
+  114: "F3",
+  115: "F4",
+  116: "F5",
+  117: "F6",
+  118: "F7",
+  119: "F8",
+  120: "F9",
+  121: "F10",
+  122: "F11",
+  123: "F12",
+  186: ";",
+  188: "<",
+  189: "-",
+  187: "+",
+  190: ">",
+  191: "/",
+  192: "~",
+  219: "[",
+  220: "\\",
+  221: "]",
+  222: "'"
+};
 
 function recordKeyPress(e) {
   if (
-    (e.keyCode >= 48 && e.keyCode <= 57)    // Numbers 0-9
-    || (e.keyCode >= 65 && e.keyCode <= 90) // Letters A-Z
-    || keyCodeAliases[e.keyCode]            // Other character keys
+    (e.keyCode >= 48 && e.keyCode <= 57) || // Numbers 0-9
+    (e.keyCode >= 65 && e.keyCode <= 90) || // Letters A-Z
+    keyCodeAliases[e.keyCode] // Other character keys
   ) {
-    e.target.value = keyCodeAliases[e.keyCode] || String.fromCharCode(e.keyCode);
+    e.target.value =
+      keyCodeAliases[e.keyCode] || String.fromCharCode(e.keyCode);
     e.target.keyCode = e.keyCode;
 
     e.preventDefault();
     e.stopPropagation();
-  } else if (e.keyCode === 8) { // Clear input when backspace pressed
-    e.target.value = '';
-  } else if (e.keyCode === 27) { // When esc clicked, clear input
-    e.target.value = 'null';
+  } else if (e.keyCode === 8) {
+    // Clear input when backspace pressed
+    e.target.value = "";
+  } else if (e.keyCode === 27) {
+    // When esc clicked, clear input
+    e.target.value = "null";
     e.target.keyCode = null;
   }
-};
+}
 
 function inputFilterNumbersOnly(e) {
   var char = String.fromCharCode(e.keyCode);
@@ -103,18 +104,20 @@ function inputFilterNumbersOnly(e) {
     e.preventDefault();
     e.stopPropagation();
   }
-};
+}
 
 function inputFocus(e) {
-   e.target.value = "";
-};
+  e.target.value = "";
+}
 
 function inputBlur(e) {
-  e.target.value = keyCodeAliases[e.target.keyCode] || String.fromCharCode(e.target.keyCode);
-};
+  e.target.value =
+    keyCodeAliases[e.target.keyCode] || String.fromCharCode(e.target.keyCode);
+}
 
 function updateShortcutInputText(inputId, keyCode) {
-  document.getElementById(inputId).value = keyCodeAliases[keyCode] || String.fromCharCode(keyCode);
+  document.getElementById(inputId).value =
+    keyCodeAliases[keyCode] || String.fromCharCode(keyCode);
   document.getElementById(inputId).keyCode = keyCode;
 }
 
@@ -124,7 +127,7 @@ function updateCustomShortcutInputText(inputItem, keyCode) {
 }
 
 // List of custom actions for which customValue should be disabled
-var customActionsNoValues=["pause","muted","mark","jump","display", "adaptive"];
+var customActionsNoValues = ["pause", "muted", "mark", "jump", "display"];
 
 function add_shortcut() {
   var html = `<select class="customDo">
@@ -139,20 +142,22 @@ function add_shortcut() {
     <option value="mark">Set marker</option>
     <option value="jump">Jump to marker</option>
     <option value="display">Show/hide controller</option>
-    <option value="adaptive">Adaptive speed</option>
-    </select> 
-    <input class="customKey" type="text" placeholder="press a key"/> 
-    <input class="customValue" type="text" placeholder="value (0.10)"/> 
+    </select>
+    <input class="customKey" type="text" placeholder="press a key"/>
+    <input class="customValue" type="text" placeholder="value (0.10)"/>
     <select class="customForce">
     <option value="false">Do not disable website key bindings</option>
-    <option value="true">Disable websites key bindings</option>
+    <option value="true">Disable website key bindings</option>
     </select>
     <button class="removeParent">X</button>`;
-  var div = document.createElement('div');
-  div.setAttribute('class', 'row customs');
+  var div = document.createElement("div");
+  div.setAttribute("class", "row customs");
   div.innerHTML = html;
   var customs_element = document.getElementById("customs");
-  customs_element.insertBefore(div, customs_element.children[customs_element.childElementCount - 1]);
+  customs_element.insertBefore(
+    div,
+    customs_element.children[customs_element.childElementCount - 1]
+  );
 }
 
 function createKeyBindings(item) {
@@ -160,28 +165,37 @@ function createKeyBindings(item) {
   const key = item.querySelector(".customKey").keyCode;
   const value = Number(item.querySelector(".customValue").value);
   const force = item.querySelector(".customForce").value;
-  const predefined = !!item.id;//item.id ? true : false;
+  const predefined = !!item.id; //item.id ? true : false;
 
-  keyBindings.push({action: action, key: key, value: value, force: force, predefined: predefined});
+  keyBindings.push({
+    action: action,
+    key: key,
+    value: value,
+    force: force,
+    predefined: predefined
+  });
 }
 
 // Validates settings before saving
 function validate() {
   var valid = true;
-  var status = document.getElementById('status');
-  document.getElementById('blacklist').value.split("\n").forEach(match => {
-    match = match.replace(regStrip,'')
-    if (match.startsWith('/')) {
-      try {
-        var regexp = new RegExp(match);
-      } catch(err) {
-        status.textContent = 'Error: Invalid Regex: ' + match
-                           + '. Unable to save';
-        valid = false;
-        return;
+  var status = document.getElementById("status");
+  document
+    .getElementById("blacklist")
+    .value.split("\n")
+    .forEach((match) => {
+      match = match.replace(regStrip, "");
+      if (match.startsWith("/")) {
+        try {
+          var regexp = new RegExp(match);
+        } catch (err) {
+          status.textContent =
+            "Error: Invalid blacklist regex: " + match + ". Unable to save";
+          valid = false;
+          return;
+        }
       }
-    }
-  })
+    });
   return valid;
 }
 
@@ -191,47 +205,70 @@ function save_options() {
     return;
   }
   keyBindings = [];
-  Array.from(document.querySelectorAll(".customs")).forEach(item => createKeyBindings(item)); // Remove added shortcuts
+  Array.from(document.querySelectorAll(".customs")).forEach((item) =>
+    createKeyBindings(item)
+  ); // Remove added shortcuts
 
-  var rememberSpeed = document.getElementById('rememberSpeed').checked;
-  var audioBoolean = document.getElementById('audioBoolean').checked;
-  var enabled = document.getElementById('enabled').checked;
-  var startHidden = document.getElementById('startHidden').checked;
-  var controllerOpacity = document.getElementById('controllerOpacity').value;
-  var blacklist     = document.getElementById('blacklist').value;
+  var rememberSpeed = document.getElementById("rememberSpeed").checked;
+  var audioBoolean = document.getElementById("audioBoolean").checked;
+  var enabled = document.getElementById("enabled").checked;
+  var startHidden = document.getElementById("startHidden").checked;
+  var controllerOpacity = document.getElementById("controllerOpacity").value;
+  var blacklist = document.getElementById("blacklist").value;
 
-  chrome.storage.sync.remove(["resetSpeed", "speedStep", "fastSpeed", "rewindTime", "advanceTime", "resetKeyCode", "slowerKeyCode", "fasterKeyCode", "rewindKeyCode", "advanceKeyCode", "fastKeyCode"]);
-  chrome.storage.sync.set({
-    rememberSpeed:  rememberSpeed,
-    audioBoolean:  audioBoolean,
-    enabled:  enabled,
-    startHidden:    startHidden,
-    controllerOpacity:    controllerOpacity,
-    keyBindings:    keyBindings,
-    blacklist:      blacklist.replace(regStrip,'')
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 1000);
-  });
+  chrome.storage.sync.remove([
+    "resetSpeed",
+    "speedStep",
+    "fastSpeed",
+    "rewindTime",
+    "advanceTime",
+    "resetKeyCode",
+    "slowerKeyCode",
+    "fasterKeyCode",
+    "rewindKeyCode",
+    "advanceKeyCode",
+    "fastKeyCode"
+  ]);
+  chrome.storage.sync.set(
+    {
+      rememberSpeed: rememberSpeed,
+      audioBoolean: audioBoolean,
+      enabled: enabled,
+      startHidden: startHidden,
+      controllerOpacity: controllerOpacity,
+      keyBindings: keyBindings,
+      blacklist: blacklist.replace(regStrip, "")
+    },
+    function () {
+      // Update status to let user know options were saved.
+      var status = document.getElementById("status");
+      status.textContent = "Options saved";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 1000);
+    }
+  );
 }
 
 // Restores options from chrome.storage
 function restore_options() {
-  chrome.storage.sync.get(tcDefaults, function(storage) {
-    document.getElementById('rememberSpeed').checked = storage.rememberSpeed;
-    document.getElementById('audioBoolean').checked = storage.audioBoolean;
-    document.getElementById('enabled').checked = storage.enabled;
-    document.getElementById('startHidden').checked = storage.startHidden;
-    document.getElementById('controllerOpacity').value = storage.controllerOpacity;
-    document.getElementById('blacklist').value = storage.blacklist;
+  chrome.storage.sync.get(tcDefaults, function (storage) {
+    document.getElementById("rememberSpeed").checked = storage.rememberSpeed;
+    document.getElementById("audioBoolean").checked = storage.audioBoolean;
+    document.getElementById("enabled").checked = storage.enabled;
+    document.getElementById("startHidden").checked = storage.startHidden;
+    document.getElementById("controllerOpacity").value =
+      storage.controllerOpacity;
+    document.getElementById("blacklist").value = storage.blacklist;
 
     // ensure that there is a "display" binding for upgrades from versions that had it as a separate binding
-    if(storage.keyBindings.filter(x => x.action == "display").length == 0){
-      storage.keyBindings.push({ action: "display", value: 0, force: false, predefined: true });
+    if (storage.keyBindings.filter((x) => x.action == "display").length == 0) {
+      storage.keyBindings.push({
+        action: "display",
+        value: 0,
+        force: false,
+        predefined: true
+      });
     }
 
     for (let i in storage.keyBindings) {
@@ -239,27 +276,36 @@ function restore_options() {
       if (item.predefined) {
         //do predefined ones because their value needed for overlay
         // document.querySelector("#" + item["action"] + " .customDo").value = item["action"];
-        if (item["action"] == "display" && typeof (item["key"]) === "undefined"){
+        if (item["action"] == "display" && typeof item["key"] === "undefined") {
           item["key"] = storage.displayKeyCode || tcDefaults.displayKeyCode; // V
         }
 
         if (customActionsNoValues.includes(item["action"]))
-          document.querySelector("#" + item["action"] + " .customValue").disabled = true;
+          document.querySelector(
+            "#" + item["action"] + " .customValue"
+          ).disabled = true;
 
-        updateCustomShortcutInputText(document.querySelector("#" + item["action"] + " .customKey"), item["key"]);
-        document.querySelector("#" + item["action"] + " .customValue").value = item["value"];
-        document.querySelector("#" + item["action"] + " .customForce").value = item["force"];
-      }
-      else {
+        updateCustomShortcutInputText(
+          document.querySelector("#" + item["action"] + " .customKey"),
+          item["key"]
+        );
+        document.querySelector("#" + item["action"] + " .customValue").value =
+          item["value"];
+        document.querySelector("#" + item["action"] + " .customForce").value =
+          item["force"];
+      } else {
         // new ones
         add_shortcut();
-        const dom = document.querySelector(".customs:last-of-type")
+        const dom = document.querySelector(".customs:last-of-type");
         dom.querySelector(".customDo").value = item["action"];
 
         if (customActionsNoValues.includes(item["action"]))
           dom.querySelector(".customValue").disabled = true;
 
-        updateCustomShortcutInputText(dom.querySelector(".customKey"), item["key"]);
+        updateCustomShortcutInputText(
+          dom.querySelector(".customKey"),
+          item["key"]
+        );
         dom.querySelector(".customValue").value = item["value"];
         dom.querySelector(".customForce").value = item["force"];
       }
@@ -268,55 +314,63 @@ function restore_options() {
 }
 
 function restore_defaults() {
-  chrome.storage.sync.set(tcDefaults, function() {
+  chrome.storage.sync.set(tcDefaults, function () {
     restore_options();
-    document.querySelectorAll(".removeParent").forEach(button => button.click()); // Remove added shortcuts
+    document
+      .querySelectorAll(".removeParent")
+      .forEach((button) => button.click()); // Remove added shortcuts
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Default options restored';
-    setTimeout(function() {
-      status.textContent = '';
+    var status = document.getElementById("status");
+    status.textContent = "Default options restored";
+    setTimeout(function () {
+      status.textContent = "";
     }, 1000);
   });
 }
 
 function show_experimental() {
-  document.querySelectorAll(".customForce").forEach(item => item.style.display = 'inline-block');
+  document
+    .querySelectorAll(".customForce")
+    .forEach((item) => (item.style.display = "inline-block"));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   restore_options();
 
-  document.getElementById('save').addEventListener('click', save_options);
-  document.getElementById('add').addEventListener('click', add_shortcut);
-  document.getElementById('restore').addEventListener('click', restore_defaults);
-  document.getElementById('experimental').addEventListener('click', show_experimental);
+  document.getElementById("save").addEventListener("click", save_options);
+  document.getElementById("add").addEventListener("click", add_shortcut);
+  document
+    .getElementById("restore")
+    .addEventListener("click", restore_defaults);
+  document
+    .getElementById("experimental")
+    .addEventListener("click", show_experimental);
 
   function eventCaller(event, className, funcName) {
     if (!event.target.classList.contains(className)) {
-      return
+      return;
     }
     funcName(event);
   }
 
-  document.addEventListener('keypress', (event) => {
-    eventCaller(event, "customValue", inputFilterNumbersOnly)
+  document.addEventListener("keypress", (event) => {
+    eventCaller(event, "customValue", inputFilterNumbersOnly);
   });
-  document.addEventListener('focus', (event) => {
-    eventCaller(event, "customKey", inputFocus)
+  document.addEventListener("focus", (event) => {
+    eventCaller(event, "customKey", inputFocus);
   });
-  document.addEventListener('blur', (event) => {
-    eventCaller(event, "customKey", inputBlur)
+  document.addEventListener("blur", (event) => {
+    eventCaller(event, "customKey", inputBlur);
   });
-  document.addEventListener('keydown', (event) => {
-    eventCaller(event, "customKey", recordKeyPress)
+  document.addEventListener("keydown", (event) => {
+    eventCaller(event, "customKey", recordKeyPress);
   });
-  document.addEventListener('click', (event) => {
+  document.addEventListener("click", (event) => {
     eventCaller(event, "removeParent", function () {
-      event.target.parentNode.remove()
-    })
+      event.target.parentNode.remove();
+    });
   });
-  document.addEventListener('change', (event) => {
+  document.addEventListener("change", (event) => {
     eventCaller(event, "customDo", function () {
       if (customActionsNoValues.includes(event.target.value)) {
         event.target.nextElementSibling.nextElementSibling.disabled = true;
@@ -324,6 +378,6 @@ document.addEventListener('DOMContentLoaded', function () {
       } else {
         event.target.nextElementSibling.nextElementSibling.disabled = false;
       }
-    })
+    });
   });
-})
+});
