eba2aaa743d777599a34d771b8402f0d1a658fb0
Paul Irish <commits@paul.irish>
2021-01-20 22:41:16 -0800

    minor reorder

--- inject.js
+++ inject.js
@@ -272,8 +272,8 @@ function defineVideoController() {
   tc.videoController.prototype.initializeControls = function () {
     log("initializeControls Begin", 5);
     const document = this.video.ownerDocument;
-    const rect = this.video.getBoundingClientRect();
     const speed = this.video.playbackRate.toFixed(2);
+    const rect = this.video.getBoundingClientRect();
     const top = Math.max(rect.top, 0) + "px";
     const left = Math.max(rect.left, 0) + "px";
 
