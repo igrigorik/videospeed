a25373f1d831fe06430c2e9e87dc1bd1aabd25b1
Paul Irish <paulirish@users.noreply.github.com>
2021-01-25 22:04:37 -0800

    fix control position on hulu w/ getBoundingClientRect  (#752)
    * fix control position on hulu w/ getBoundingClientRect instead of offsetLeft/Top
    
    * minor reorder

--- inject.js
+++ inject.js
@@ -271,10 +271,11 @@ function defineVideoController() {
 
   tc.videoController.prototype.initializeControls = function () {
     log("initializeControls Begin", 5);
-    var document = this.video.ownerDocument;
-    var speed = this.video.playbackRate.toFixed(2),
-      top = Math.max(this.video.offsetTop, 0) + "px",
-      left = Math.max(this.video.offsetLeft, 0) + "px";
+    const document = this.video.ownerDocument;
+    const speed = this.video.playbackRate.toFixed(2);
+    const rect = this.video.getBoundingClientRect();
+    const top = Math.max(rect.top, 0) + "px";
+    const left = Math.max(rect.left, 0) + "px";
 
     log("Speed variable set to: " + speed, 5);
 
