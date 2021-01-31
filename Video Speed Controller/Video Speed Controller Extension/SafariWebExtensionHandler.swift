19a0810246523306f389724f4cb9a9bdfa415dbf
yexulei <yexulei@bytedance.com>
2021-01-31 16:55:38 +0800

    Use xcrun safari-web-extension-converter to make it Safari-Compatible

---
+++ Video Speed Controller/Video Speed Controller Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Video Speed Controller Extension
+//
+//  Created by Kyle on 2021/1/31.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
