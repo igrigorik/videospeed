2c7196261a4da808afbd9b7ac2ce0f2d6f0dea42
Lakpro <lakshaysetia123@gmail.com>
2021-08-31 21:30:11 +0530

    brings controller to top left

--- inject.js
+++ inject.js
@@ -274,8 +274,8 @@ function defineVideoController() {
     const document = this.video.ownerDocument;
     const speed = this.video.playbackRate.toFixed(2);
     const rect = this.video.getBoundingClientRect();
-    const top = Math.max(rect.top, 0) + "px";
-    const left = Math.max(rect.left, 0) + "px";
+    const top = Math.min(rect.top, 0) + "px";
+    const left = Math.min(rect.left, 0) + "px";
 
     log("Speed variable set to: " + speed, 5);
 
