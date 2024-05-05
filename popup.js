9af9acd2c93c8c1d5785dc33c19bd6453a599149
Boon Kai <bk7312@users.noreply.github.com>
2024-05-05 11:52:16 +0800

    fix(popup.js): fix setIcon error using action
    Manifest V3 uses `action` instead of `browserAction`. Reference: https://developer.chrome.com/docs/extensions/reference/api/action#method-setIcon

--- popup.js
+++ popup.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#disable").classList.toggle("hide", !enabled);
 
     const suffix = `${enabled ? "" : "_disabled"}.png`;
-    chrome.browserAction.setIcon({
+    chrome.action.setIcon({
       path: {
         "19": "icons/icon19" + suffix,
         "38": "icons/icon38" + suffix,
