3aee7b4b1c5742045743ed1c76506387d1921fc7
Ilya Grigorik <ilya@grigorik.com>
2025-06-04 23:17:36 -0700

    fix spacing

--- background.js
+++ background.js
@@ -1,53 +1,53 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'slow-down-01',
-    title: 'Slow down(x0.1)',
+    title: 'Slow down (x0.1)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'slow-down-02',
-    title: 'Slow down(x0.2)',
+    title: 'Slow down (x0.2)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'slow-down-04',
-    title: 'Slow down(x0.4)',
+    title: 'Slow down (x0.4)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'slow-down-06',
-    title: 'Slow down(x0.6)',
+    title: 'Slow down (x0.6)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'slow-down-08',
-    title: 'Slow down(x0.8)',
+    title: 'Slow down (x0.8)',
     contexts: ['all']
   });
 
   chrome.contextMenus.create({
     id: 'speed-up-12',
-    title: 'Speed up(x1.2)',
+    title: 'Speed up (x1.2)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'speed-up-14',
-    title: 'Speed up(x1.4)',
+    title: 'Speed up (x1.4)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'speed-up-16',
-    title: 'Speed up(x1.6)',
+    title: 'Speed up (x1.6)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'speed-up-18',
-    title: 'Speed up(x1.8)',
+    title: 'Speed up (x1.8)',
     contexts: ['all']
   });
   chrome.contextMenus.create({
     id: 'speed-up-20',
-    title: 'Speed up(x2.0)',
+    title: 'Speed up (x2.0)',
     contexts: ['all']
   });
 });
