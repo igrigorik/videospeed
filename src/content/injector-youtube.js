cab2cbd6132a5e5df891b0c25ad0384166dd321a
Daniel Tang <danielzgtg.opensource@gmail.com>
2025-08-28 11:03:20 -0400

    Restrict YouTube setTimeout injector to subtitles
    Co-authored-by: Can Arslan <canarslan12@hotmail.com>

--- src/content/injector-youtube.js
+++ src/content/injector-youtube.js
@@ -1,9 +1,13 @@
 (() => {
     const oldSetTimeout = window.setTimeout;
     window.setTimeout = (f, delay = 0, ...args) => {
-        // Make YouTube update captions 4x as frequent so it can
-        // keep up with Video Speed Controller
-        delay /= 4;
+        // Changing all `setTimeout`s might cause other problems and
+        // only subtitles are not rounded so this will be enough for not breaking anything.
+        if (delay % 10 !== 0) {
+            // Make YouTube update captions 4x as frequent so it can
+            // keep up with Video Speed Controller
+            delay /= 4;
+        }
         return oldSetTimeout(f, delay, ...args);
     };
 })();
